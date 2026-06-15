e52a41fd4ff2c65c8f5bfc4197eb439d1ad41d02
alione10-lm <aliouaneoussama89@gmail.com>
2026-06-15 16:23:51 +0100

    Add products page with product listing and ProductCard integration

---
+++ app/products/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { products } from "@/data/products";
+import ProductCard from "@/components/ProductCard";
+
+const page = () => {
+    return (
+        <section className="mt-16">
+            <h1 className="text-2xl relative before:content-[''] before:absolute before:-left-2 before:top-1/2 before:transform before:-translate-y-1/2 before:w-1 before:h-6 before:bg-primary before:rounded-full  font-bold">
+                All Products
+            </h1>
+            <div className="grid grid-cols-1 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        id={product.id}
+                        name={product.name}
+                        image={product.image}
+                        category={product.category}
+                        price={product.price}
+                        slug={product.slug}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default page;
